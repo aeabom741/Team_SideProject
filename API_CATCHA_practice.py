50d7d8f88674726081671c8daf181deac3889838
Andddddddy <31921264+Andddddddy@users.noreply.github.com>
2021-06-26 10:46:34 +0800

    2021.06.26 weather update

--- API_CATCHA_practice.py
+++ API_CATCHA_practice.py
@@ -5,12 +5,6 @@ url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorizati
 r = requests.get(url)
 dict = r.json()
 
-# city
-dict["records"]["locations"][0]["location"][0]["locationName"]
-
-# weather elements
-dict["records"]["locations"][0]["location"][0]["weatherElement"]
-
 # 資料類型
 # 0     PoP12h                  12小時降雨機率
 # 1     T                       平均溫度
@@ -45,20 +39,13 @@ for i in city_level:
             value_for_data = m["elementValue"][0]["value"]
             df.loc[len(df)] = [city_name, weather_element, start_time, end_time, value_for_data]
 
-            
+# df.to_excel("/Users/shijunliao/Desktop/Python/side_project/weather/weather.xlsx", index=False)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+citys = [("新竹縣"), ("金門縣"), ("苗栗縣"), ("新北市"), ("宜蘭縣"), ("雲林縣"), ("臺南市"), ("高雄市"), ("彰化縣"), ("臺北市"), ("南投縣"), ("澎湖縣"), ("基隆市"), ("桃園市"), ("花蓮縣"), \
+        ("連江縣"), ("臺東縣"), ("嘉義市"), ("嘉義縣"), ("屏東縣"), ("臺中市"), ("新竹市")]
+weatherElements = [("12小時降雨機率"), ("平均溫度"), ("平均相對濕度"), ("最小舒適度指數"), ("最大風速"), ("最高體感溫度"), ("天氣現象"), ("最大舒適度指數"), ("最低溫度"), ("紫外線指數"), \
+        ("天氣預報綜合描述"), ("最低體感溫度"), ("最高溫度"), ("風向"), ("平均露點溫度")]
+for city in citys:
+    for weatherElement in weatherElements:
+        df_export = df[(df["Location Name"] == city) & (df["Weather Element"] == weatherElement)]
+        df_export.to_excel(f"/Users/shijunliao/Desktop/Python/side_project/weather/{weatherElement}/{weatherElement}-{city}.xlsx", index=False)
