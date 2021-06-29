01dea4e362d9f9a93c6e35434556d65d7bb0b719
Andddddddy <31921264+Andddddddy@users.noreply.github.com>
2021-06-29 21:28:24 +0800

    2021.06.29 API_CATCHA update

--- API_CATCHA_practice_Andy.py
+++ API_CATCHA_practice_Andy.py
@@ -24,15 +24,13 @@ dict = r.json()
 
 # 最小舒適度指數、最大風速、天氣現象、最大舒適度指數、紫外線指數 的value
 
-# 正式版
-df = pd.DataFrame(columns=["Location Name", "Weather Element", "Start Time", "End Time", "Value"])
+# save data
 city_level = dict["records"]["locations"][0]["location"]
 for i in city_level:
-    # print(i["locationName"])
     city_name = i["locationName"]
     weatherElement_level = i["weatherElement"]
     for s in weatherElement_level:
-        # print(s["description"])
+        df = pd.DataFrame(columns=["Location Name", "Weather Element", "Start Time", "End Time", "Value"])
         weather_element = s["description"]
         time_level = s["time"]
         for m in time_level:
@@ -43,18 +41,5 @@ for i in city_level:
             else:
                 value_for_data = m["elementValue"][0]["value"]
             df.loc[len(df)] = [city_name, weather_element, start_time, end_time, value_for_data]
-print("data save in dataframe")
-# df.to_excel("/Users/shijunliao/Desktop/Python/side_project/weather/weather.xlsx", index=False)
-
-
-
-citys = ["新竹縣", "金門縣", "苗栗縣", "新北市", "宜蘭縣", "雲林縣", "臺南市", "高雄市", "彰化縣", "臺北市", "南投縣", "澎湖縣", "基隆市", "桃園市", "花蓮縣", \
-        "連江縣", "臺東縣", "嘉義市", "嘉義縣", "屏東縣", "臺中市", "新竹市"]
-weatherElements = ["12小時降雨機率", "平均溫度", "平均相對濕度", "最小舒適度指數", "最大風速", "最高體感溫度", "天氣現象", "最大舒適度指數", "最低溫度", "紫外線指數", \
-        "天氣預報綜合描述", "最低體感溫度", "最高溫度", "風向", "平均露點溫度"]
-for city in citys:
-    for weatherElement in weatherElements:
-        df_export = df[(df["Location Name"] == city) & (df["Weather Element"] == weatherElement)]
-        df_export.to_excel(f"/Users/shijunliao/Desktop/Python/side_project/weather/weather_data/{city}/{weatherElement}.xlsx", index=False)
-    print(f"{city} Done")
-print("Completely Done")
+        df.to_excel(f"/Users/shijunliao/Desktop/Python/side_project/weather/weather_data/{city_name}/{weather_element}.xlsx", index=False)
+print("All Data Download Done")
