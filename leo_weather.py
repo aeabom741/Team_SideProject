5d24b2a9c992a9da223d18a396856a4369a8941f
leo <aeabom741@gmail.com>
2021-06-29 21:24:09 +0800

    test

--- leo_weather.py
+++ leo_weather.py
@@ -1,4 +1,3 @@
-from time import time
 import pandas as pd
 import requests
 
@@ -7,11 +6,12 @@ r = requests.get(url)
 dict = r.json()
 
 city_level = dict["records"]["locations"][0]["location"]
-df = pd.DataFrame(columns=["Location Name","description","Value","Measure","Start Time","End Time" ])
+df = pd.DataFrame(columns=["Location Name","description","Value","Start Time","End Time" ])
 
 
 
-model_selection = int(input("Chose youe model:"))
+
+model_selection = int(input("Choose your model:"))
 '''
 Select you want data type
 model_selection = 1 (download you want to know city data)
@@ -54,6 +54,7 @@ if model_selection == 1:
                         export.to_excel(f"/Users/lvjiawei/Documents/weather/{city_name}/{description}.xlsx",index=False)
                         print("Complete!")
 
+
 #model_selection = 2
 elif model_selection == 2:
     print("Model 2 Start")
@@ -62,27 +63,20 @@ elif model_selection == 2:
         weatherElement = i["weatherElement"]
         for j in weatherElement:
             element_name = j["elementName"]
-            description = j["description"]
+            descriptions = j["description"]
             value_time = j["time"]
             for k in value_time:
                 start_time = k["startTime"]
                 end_time = k["endTime"]
                 element_value = k["elementValue"]
-                for v in element_value:
-                    value = v["value"]
-                    measure = v["measures"]
-                    df.loc[len(df)] = [city_name,description,value,measure,start_time,end_time]
+                if descriptions in ["最小舒適度指數", "最大風速", "最大舒適度指數", "紫外線指數"]:
+                    value = k["elementValue"][1]["value"]
+                else:
+                    value = k["elementValue"][0]["value"]
+                df.loc[len(df)] = [city_name, descriptions ,value , start_time , end_time]
+                df_export = df[(df["Location Name"] == city_name) & (df["description"] == descriptions)]
+                df_export.to_excel(f"/Users/lvjiawei/Documents/weather/{city_name}/{descriptions}.xlsx",index=False)
 
-
-    citys = [("新竹縣"), ("金門縣"), ("苗栗縣"), ("新北市"), ("宜蘭縣"), ("雲林縣"), ("臺南市"), ("高雄市"), ("彰化縣"), ("臺北市"), ("南投縣"), ("澎湖縣"), ("基隆市"), ("桃園市"), ("花蓮縣"), \
-             ("連江縣"), ("臺東縣"), ("嘉義市"), ("嘉義縣"), ("屏東縣"), ("臺中市"), ("新竹市")]
-
-    descriptions = [("12小時降雨機率"), ("平均溫度"), ("平均相對濕度"), ("最小舒適度指數"), ("最大風速"), ("最高體感溫度"), ("天氣現象"), ("最大舒適度指數"), ("最低溫度"), ("紫外線指數"), \
-                    ("天氣預報綜合描述"), ("最低體感溫度"), ("最高溫度"), ("風向"), ("平均露點溫度")]
-    for city in citys:
-        for description in descriptions:
-            df_export = df[(df["Location Name"] == city) & (df["description"] == description)]
-            df_export.to_excel(f"/Users/lvjiawei/Documents/weather/{city}/{description}.xlsx",index=False)
-    print("Complete!")
 else:
     print("model_selection type Error")
+
